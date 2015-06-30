18c1ef02a2b7a47814f5e7ab9c92822ef65862fd
dane <daneshih1125@gmail.com>
2015-06-30 15:09:09 +0800

    unattended apt-get install

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
 set_bashrc() {
 	set_o_vi=$(grep -E "set\s*-o\s*vi" ~/.bashrc)
 	[ "$set_o_vi" = "" ] && echo 'set -o vi' >> ~/.bashrc
@@ -6,7 +9,7 @@ set_bashrc() {
 
 
 setup_vim() {
-	sudo apt-get install vim cscope ctags
+	sudo apt-get -y install vim cscope ctags
 	[ -d "/tmp/vim" ] && rm -rf /tmp/vim
 	git clone https://github.com/daneshih1125/my-vim.git /tmp/vim
 	cp -a /tmp/vim/.vim  /tmp/vim/.vimrc ~/
@@ -15,14 +18,14 @@ setup_vim() {
 }
 
 apt_tools() {
-	sudo apt-get install apt-file 
+	sudo apt-get -y install apt-file 
 	apt-file update
 }
 
 ubuntu_kernel_env() {
 	sudo add-apt-repository ppa:canonical-kernel-team/ppa
 	sudo apt-get update
-	sudo apt-get install dpkg-dev libncurses5-dev
+	sudo apt-get -y install dpkg-dev libncurses5-dev
 }
 
 apt_tools
