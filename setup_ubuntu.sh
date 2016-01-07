2854691cb795ca6d73e04b51a5d9dd0317b457c0
yuan <daneshih1125@gmail.com>
2016-01-07 17:25:12 +0800

    add git ls

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -12,6 +12,7 @@ setup_git() {
 	git config --global user.username "Dane"
 	git config --global user.email "daneshih1125@gmail.com"
 	git config --global alias.ll "log --all --graph --decorate --oneline --simplify-by-decoration"
+	git config --global alias.ls "log --name-status"
 }
 
 setup_vim() {
