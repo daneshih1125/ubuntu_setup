14a79ccd9f8db94735f84a1acb91c2cf678e127d
Dane <daneshih1125@gmail.com>
2019-07-11 14:56:06 +0000

    vim8

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -19,7 +19,7 @@ setup_git() {
 setup_docker() {
 	sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 		$(lsb_release -cs) \
 		stable"
 	sudo apt-get update
@@ -27,6 +27,9 @@ setup_docker() {
 }
 
 setup_vim() {
+	# vim8
+	sudo add-apt-repository -y ppa:jonathonf/vim
+	sudo apt-get update
 	sudo apt-get -y install vim vim-gtk cscope ctags
 	# YouCompleteMe
 	sudo apt-get -y install python-dev
